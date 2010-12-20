e948fa9d12c333ba0668637de7391d4307459b80
Neil Edwards <neil_edwards@buffer.uk.com>
2010-12-20 23:23:07 +0000

    Added UserAccount and userVO for account

---
+++ UserAccount.h
@@ -0,0 +1,62 @@
+//
+//  UserAccount.h
+//  CycleStreets
+//
+//  Created by Neil Edwards on 17/12/2010.
+//  Copyright 2010 CycleStreets Ltd. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "SynthesizeSingleton.h"
+#import "FrameworkObject.h"
+#import "UserVO.h"
+#import "MBProgressHUD.h"
+
+#define NMUSERSTATEFILE @"NMUserState.user"
+#define kUSERSTATEARCHIVEKEY @"UserStateArchiveKey"
+
+
+enum  {
+	kUserAccountLoggedIn=0,
+	kUserAccountNotLoggedIn=1,
+	kUserAccountPassword=2,
+	kUserAccountCredentialsExist=3, // we have username/password but user has not got autologin enabled
+	kUserAccountNone=4
+};
+typedef int UserAccountMode;
+
+@interface UserAccount : FrameworkObject <MBProgressHUDDelegate>{
+	UserVO				*user;
+	NSString			*userPassword;
+	NSString			*userName;
+	BOOL				isRegistered; // 
+	NSString			*sessionToken;
+	NSString			*deviceID;
+	
+	UserAccountMode		accountMode;
+	
+	MBProgressHUD					*HUD;
+}
+@property (nonatomic, retain)			UserVO *user;
+@property (nonatomic, retain)			NSString *userPassword;
+@property (nonatomic, retain)			NSString *userName;
+@property (nonatomic)			BOOL isRegistered;
+@property (nonatomic, retain)			NSString *sessionToken;
+@property (nonatomic, retain)			NSString *deviceID;
+@property (nonatomic)			UserAccountMode accountMode;
+
+SYNTHESIZE_SINGLETON_FOR_CLASS_HEADER(UserAccount)
+
+-(void)loginUserWithUserName:(NSString*)name andPassword:(NSString*)password;
+-(void)registerUserWithUserName:(NSString*)name andPassword:(NSString*)password visibleName:(NSString*)visiblename email:(NSString*)email;
+
+-(void)loginExistingUser;
+-(void)retrievePasswordForUser:(NSString*)email;
+-(BOOL)hasSessionToken;
+-(void)logoutUser;
+-(void)resetUserAccount;
+-(void)updateAutoLoginPreference:(BOOL)value;
+-(void)removeHUD;
+-(void)showProgressHUDWithMessage:(NSString*)message;
+@end
