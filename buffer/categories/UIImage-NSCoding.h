a495a215cbd3d5d269d7fbeb266899f059556020
neilkachu <neilkachu@mac.com>
2011-03-03 22:02:18 +0000

    Part 1 of reorganisation
    Delete all files from root

--- buffer/categories/UIImage-NSCoding.h
+++
@@ -1,9 +0,0 @@
-//
-//  UIImage-NSCoding.h
-
-#import <Foundation/Foundation.h>
-
-@interface UIImageNSCoding <NSCoding>
-- (id)initWithCoder:(NSCoder *)decoder;
-- (void)encodeWithCoder:(NSCoder *)encoder;
-@end
