203975537af195d2064bc95a4a051680f76eff50
Neil Edwards <neil_edwards@mac.com>
2012-11-13 10:54:03 +0000

    b215
    + Added some missing x2 images
    + fixed styles for RouteSegment toolbar buttons states

--- src/controllers/Itinerary/RouteSegmentViewController.m
+++ src/controllers/Itinerary/RouteSegmentViewController.m
@@ -477,6 +477,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 		
 		footerIsHidden=NO;
 	}
+	
+	if(footerIsHidden==NO){
+		self.infoButton.style=UIBarButtonItemStyleDone;
+	}else {
+		self.infoButton.style=UIBarButtonItemStyleBordered;
+	}
 }
 
 
