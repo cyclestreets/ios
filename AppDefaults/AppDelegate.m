9f750d86eddada4cc97877a2279ca8142f8e1346
Neil Edwards <neil_edwards@mac.com>
2014-01-25 17:46:18 +0000

    + updated Tf key

--- AppDefaults/AppDelegate.m
+++ AppDefaults/AppDelegate.m
@@ -77,7 +77,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 	
 	
 	#if defined (CONFIGURATION_Adhoc)
-	[TestFlight takeOff:@"66d1beaa-3747-4893-8146-93c7003bc24f"]; //66d1beaa-3747-4893-8146-93c7003bc24f
+	[TestFlight takeOff:@"e1447ee8-f43c-4f83-87b1-54709700a8b6"]; //66d1beaa-3747-4893-8146-93c7003bc24f
 	#endif
 	
 	[Crashlytics startWithAPIKey:@"ea3a63e4bd4d920df480d1f6635e7e38b20e6634"];
