39199ecad230583261fcaa90a18112409f6c2db4
Neil Edwards <neil_edwards@mac.com>
2014-09-17 15:19:48 +0100

    + Updated Pods: BlocksKit >2.2.5 to fix OS8 issue with Actionsheets

--- Podfile
+++ Podfile
@@ -1,8 +1,10 @@
+platform :ios, "7"
+
 pod 'AFNetworking', '~> 2.0'
 pod 'JVFloatLabeledTextField'
 pod 'SFHFKeychainUtils'
 pod 'RegexKitLite'
-pod 'BlocksKit'
+pod 'BlocksKit', '~> 2.2.5'
 pod 'ViewDeck', '~> 2.2.11'
 pod 'FMMoveTableView'
 pod 'TouchJSON'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -58,37 +58,37 @@
 #define COCOAPODS_POD_AVAILABLE_BlocksKit
 #define COCOAPODS_VERSION_MAJOR_BlocksKit 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit 5
 
 // BlocksKit/All
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_All
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_All 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_All 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_All 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_All 5
 
 // BlocksKit/Core
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_Core
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_Core 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_Core 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_Core 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_Core 5
 
 // BlocksKit/DynamicDelegate
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_DynamicDelegate
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_DynamicDelegate 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_DynamicDelegate 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_DynamicDelegate 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_DynamicDelegate 5
 
 // BlocksKit/MessageUI
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_MessageUI
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_MessageUI 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_MessageUI 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_MessageUI 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_MessageUI 5
 
 // BlocksKit/UIKit
 #define COCOAPODS_POD_AVAILABLE_BlocksKit_UIKit
 #define COCOAPODS_VERSION_MAJOR_BlocksKit_UIKit 2
 #define COCOAPODS_VERSION_MINOR_BlocksKit_UIKit 2
-#define COCOAPODS_VERSION_PATCH_BlocksKit_UIKit 3
+#define COCOAPODS_VERSION_PATCH_BlocksKit_UIKit 5
 
 // FMMoveTableView
 #define COCOAPODS_POD_AVAILABLE_FMMoveTableView
