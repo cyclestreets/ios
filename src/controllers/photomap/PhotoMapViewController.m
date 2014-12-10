82700585b4e361c4cb0a7146863ffc06fdf6deb6
Neil Edwards <neil_edwards@mac.com>
2014-12-10 09:09:27 +0000

    + reset all video playback code to default

--- src/controllers/photomap/PhotoMapViewController.m
+++ src/controllers/photomap/PhotoMapViewController.m
@@ -233,10 +233,6 @@ static NSString *const LOCATIONSUBSCRIBERID=@"PhotoMap";
 				lv.dataProvider=photoEntry;
 				[self presentModalViewController:lv animated:YES];
 				
-//				PhotoMapVideoLocationViewController *lv = [[PhotoMapVideoLocationViewController alloc] initWithNibName:[PhotoMapVideoLocationViewController nibName] bundle:nil];
-//				lv.dataProvider=photoEntry;
-//				[self presentModalViewController:lv animated:YES];
-				
 			}
 		break;
 				
